0c3619eda80c16293b5073002f9a54581a61b361
jdacewicz <jakub.dacewicz99@gmail.com>
2023-03-02 15:10:09 +0100

    Implemented displaying creator and post image.

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -6,15 +6,17 @@ $(document).ready(function () {
         data.forEach(function (post) {
             let newPost = $('div[name="post[0]"]').eq(0).clone();
 
-            newPost.attr("name", newPost.attr("name").replace('[0]', '[' + post.id + ']'));
-            newPost.find('span').each(function () {
-                $(this).attr("name", $(this).attr("name").replace('[0]', '[' + post.id + ']'));
+            newPost.attr("name", newPost.attr("name").replace('[0]', `[${post.id}]`));
+            newPost.find('span[name^="post[0]"], div[name^="post[0]"]').each(function () {
+                $(this).attr("name", $(this).attr("name").replace('[0]', `[${post.id}]`));
             });
             $("#posts").append(newPost);
 
-            $('span[name="post[' + post.id + '].creator-identity"]').text(post.postCreator.userInformation.firstname + " " + post.postCreator.userInformation.lastname);
-            $('span[name="post[' + post.id + '].elapsed-time"]').text(post.elapsedCreationTimeMessage + " ago.");
-            $('span[name="post[' + post.id + '].content"]').text(post.content);
+            $(`div[name="post[${post.id}].image"] img`).attr("src", post.imagePath);
+            $(`div[name="post[${post.id}].creator"] img`).attr("src", post.postCreator.userInformation.profilePicturePath);
+            $(`div[name="post[${post.id}].creator"] span`).text(post.postCreator.userInformation.firstname + " " + post.postCreator.userInformation.lastname);
+            $(`span[name="post[${post.id}].elapsed-time"]`).text(post.elapsedCreationTimeMessage + " ago.");
+            $(`span[name="post[${post.id}].content"]`).text(post.content);
         })
         $('div[name="post[0]"]').remove();
         $('#posts').fadeIn("slow");
